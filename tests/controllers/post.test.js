a3b4be1a051c34edba26cc89a06fb8c6944da813
Bruno Dorota Andretto <bruno.andretto@ze.delivery>
2021-08-14 18:15:59 -0300

    Added jest and initial unit tests

---
+++ tests/controllers/post.test.js
@@ -0,0 +1,55 @@
+const postController = require('../../controllers/post.js');
+const postFixture = require('../fixtures/post.json');
+const postRepository = require('../../repository/post.js');
+
+jest.mock('../../repository/post.js')
+
+describe('findPostById', () => {
+  test('returns bad request response if invalid parameters', () => {
+    const id = 'abc123';
+    const input = `/post/${id}`;
+    const expectedResult = {code: 400, body: {message: 'Invalid parameters'}};
+
+    const result = postController.findPostById(input);
+  
+    expect(result).toStrictEqual(expectedResult);
+  });
+  
+  test('returns ok response if post exists', () => {
+    const id = '60c4f8e061f566c252bd3ef4';
+    const input = `/post/${id}`;
+    const mockResult = new Promise((resolve, reject) => {resolve()}).then(() => {return postFixture});
+    postRepository.findPostById.mockResolvedValue(mockResult);
+    const expectedResult = {code: 200, body: postFixture};
+  
+    return postController.findPostById(input).then((result) => {
+      expect(result).toStrictEqual(expectedResult);
+    });
+  });
+
+  test('returns not found response if post does not exist', () => {
+    const id = '60c4f8e061f566c252bd3ef4';
+    const input = `/post/${id}`;
+    const mockResult = new Promise((resolve, reject) => {resolve()}).then(() => {return null});
+    postRepository.findPostById.mockResolvedValue(mockResult);
+    const expectedResult = {code: 404, body: {message: 'Post not found'}};
+  
+    return postController.findPostById(input).then((result) => {
+      expect(result).toStrictEqual(expectedResult);
+    });
+  });
+
+  test('returns internal error response if exception is raised', () => {
+    const id = '60c4f8e061f566c252bd3ef4';
+    const input = `/post/${id}`;
+    const mockResult = new Promise((resolve, reject) => {resolve()}).then(() => {
+      throw new Error('Error!')
+    });
+    postRepository.findPostById.mockResolvedValue(mockResult);
+    const expectedResult = {code: 500, body: {message: 'Internal Server Error'}};
+  
+    return postController.findPostById(input).then((result) => {
+      expect(result).toStrictEqual(expectedResult);
+    });
+  });
+});
---
+++ tests/controllers/validators/id.test.js
@@ -0,0 +1,19 @@
+const idValidator = require('../../../controllers/validators/id.js');
+
+test('returns true if id has 24 digits from a through f and 0 through 9', () => {
+  const result = idValidator.validObjectId('60c4f8e061f566c252bd3ef4');
+
+  expect(result).toBe(true);
+});
+
+test('returns false if id has invalid characters', () => {
+  const result = idValidator.validObjectId('$%c4f8e061f566c252bd3ef4');
+
+  expect(result).toBe(false);
+});
+
+test('returns false if id has invalid length', () => {
+  const result = idValidator.validObjectId('60c4f8e061f566c252bd3ef4123');
+
+  expect(result).toBe(false);
+});
