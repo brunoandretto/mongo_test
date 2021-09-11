10f5253bd5cc9c6dcf9cc60c5f93c6fe6b8565b9
Bruno Dorota Andretto <bruno.andretto@ze.delivery>
2021-09-11 12:59:17 -0300

    Add database support to tests
    Fixed tests syntax

--- mongo_test_backend/__tests__/local/controllers/post.test.js
+++ mongo_test_backend/__tests__/local/controllers/post.test.js
@@ -5,51 +5,57 @@ const postRepository = require('../../../repository/post.js');
 jest.mock('../../../repository/post.js')
 
 describe('findPostById', () => {
-  test('returns bad request response if invalid parameters', () => {
-    const id = 'abc123';
-    const input = `/post/${id}`;
-    const expectedResult = {code: 400, body: {message: 'Invalid parameters'}};
+  describe('when invalid parameters', () => {
+    test('returns bad request response', () => {
+      const id = 'abc123';
+      const input = `/post/${id}`;
+      const expectedResult = {code: 400, body: {message: 'Invalid parameters'}};
 
-    const result = postController.findPostById(input);
-  
-    expect(result).toStrictEqual(expectedResult);
-  });
-  
-  test('returns ok response if post exists', () => {
-    const id = '60c4f8e061f566c252bd3ef4';
-    const input = `/post/${id}`;
-    const mockResult = new Promise((resolve, reject) => {resolve()}).then(() => {return postFixture});
-    postRepository.findPostById.mockResolvedValue(mockResult);
-    const expectedResult = {code: 200, body: postFixture};
-  
-    return postController.findPostById(input).then((result) => {
+      const result = postController.findPostById(input);
+
       expect(result).toStrictEqual(expectedResult);
     });
   });
 
-  test('returns not found response if post does not exist', () => {
-    const id = '60c4f8e061f566c252bd3ef4';
-    const input = `/post/${id}`;
-    const mockResult = new Promise((resolve, reject) => {resolve()}).then(() => {return null});
-    postRepository.findPostById.mockResolvedValue(mockResult);
-    const expectedResult = {code: 404, body: {message: 'Post not found'}};
-  
-    return postController.findPostById(input).then((result) => {
-      expect(result).toStrictEqual(expectedResult);
+  describe('when post exists', () => {
+    test('returns ok response', () => {
+      const id = '60c4f8e061f566c252bd3ef4';
+      const input = `/post/${id}`;
+      const mockResult = Promise.resolve(postFixture);
+      postRepository.findPostById.mockResolvedValue(mockResult);
+      const expectedResult = {code: 200, body: postFixture};
+
+      return postController.findPostById(input).then((result) => {
+        expect(result).toStrictEqual(expectedResult);
+      });
     });
   });
 
-  test('returns internal error response if exception is raised', () => {
-    const id = '60c4f8e061f566c252bd3ef4';
-    const input = `/post/${id}`;
-    const mockResult = new Promise((resolve, reject) => {resolve()}).then(() => {
-      throw new Error('Error!')
+  describe('when post does not exist', () => {
+    test('returns not found response', () => {
+      const id = '60c4f8e061f566c252bd3ef4';
+      const input = `/post/${id}`;
+      const mockResult = Promise.resolve(null);
+      postRepository.findPostById.mockResolvedValue(mockResult);
+      const expectedResult = {code: 404, body: {message: 'Post not found'}};
+
+      return postController.findPostById(input).then((result) => {
+        expect(result).toStrictEqual(expectedResult);
+      });
     });
-    postRepository.findPostById.mockResolvedValue(mockResult);
-    const expectedResult = {code: 500, body: {message: 'Internal Server Error'}};
-  
-    return postController.findPostById(input).then((result) => {
-      expect(result).toStrictEqual(expectedResult);
+  });
+
+  describe('when exception is raised', () => {
+    test('returns internal error response', () => {
+      const id = '60c4f8e061f566c252bd3ef4';
+      const input = `/post/${id}`;
+      const mockResult = Promise.reject(new Error('Error!'))
+      postRepository.findPostById.mockResolvedValue(mockResult);
+      const expectedResult = {code: 500, body: {message: 'Internal Server Error'}};
+
+      return postController.findPostById(input).then((result) => {
+        expect(result).toStrictEqual(expectedResult);
+      });
     });
   });
 });
